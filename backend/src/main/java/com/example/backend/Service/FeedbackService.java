ad6abfb496f62827196a38315c129b86febc66ac
azPeu <psilvaoliveira122@gmail.com>
2024-12-01 23:12:11 -0300

    Implements Feedback API

--- backend/src/main/java/com/example/backend/Service/FeedbackService.java
+++ backend/src/main/java/com/example/backend/Service/FeedbackService.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,7 +31,7 @@ public class FeedbackService {
         User user = existUser.get();
         Feedback feedback = new Feedback();
         feedback.setDescricao(feedbackDTO.getDescricao());
-        feedback.setData(feedbackDTO.getData());
+        feedback.setData(new Date()); // Define a data atual
         feedback.setUser(user);
 
         Feedback createFeedback = feedbackRepository.save(feedback);
--- mobile/app/(tabs)/home/feedback.tsx
+++ mobile/app/(tabs)/home/feedback.tsx
@@ -5,13 +5,62 @@ import {
   StyleSheet,
   TextInput,
   TouchableOpacity,
+  Modal,
 } from "react-native";
 import CustomInput from "../../../components/CustomInputs";
 import { useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import blogFetch from "../../../axios/config"
+import { ThemedText } from "@/components/ThemedText";
 export default function Feedback() {
-  const [feedback, setFeedback] = useState("");
+  const [descricao, setDescricao] = useState("");
+  const [modalActive, setModalActive] = useState(false)
+  const insertFeedback = async () => {
+    const id = await AsyncStorage.getItem('@id');
+    const token = await AsyncStorage.getItem('@token');
+
+    try {
+
+      const response = await blogFetch.post(`/feedbackAPI/createFeedback/${id}`,{
+        descricao: descricao
+      }, {
+        
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = response.data;
+      setModalActive(true)
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <SafeAreaView style={{ flex: 1 }}>
+    <Modal
+          animationType="fade"
+          transparent={true}
+          visible={modalActive} >
+          <View style={styles.outerView}>
+            <View style={styles.modalView}>
+              <ThemedText type="subtitle">
+                Muito obrigado pelo seu Feedback, graças a ele podemos melhorar sempre!
+              </ThemedText>
+              <View style={{flexDirection:'row',width:200, padding:15, justifyContent:'space-between',right:10}}>
+                <TouchableOpacity style={styles.cancelButton} 
+                onPress={() => {
+                  setModalActive(false); // Fecha o modal
+                  setDescricao('');           // Limpa o texto
+                }}>
+                  <Text style={{color:'#fff'}}>Cancelar</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </Modal>
+
+
+
       <View style={styles.container}>
         <Text style={{ fontSize: 30, textAlign: "center" }}>
           Compartilhe sua opinião conosco e nos ajude a melhorar o App!
@@ -20,6 +69,8 @@ export default function Feedback() {
         <View style={{ marginTop: 10 }}>
           <TextInput
             underlineColorAndroid="transparent"
+            value={descricao} // Vincula o estado ao TextInput
+            onChangeText={(value) => setDescricao(value)} // Atualiza o estado ao digitar
             placeholder="Digite aqui"
             placeholderTextColor="grey"
             numberOfLines={10}
@@ -34,7 +85,7 @@ export default function Feedback() {
             }}
           />
         </View>
-        <TouchableOpacity style={styles.feedbackButton}>
+        <TouchableOpacity style={styles.feedbackButton} onPress={insertFeedback}>
           <Text style={styles.buttonText}>Enviar</Text>
         </TouchableOpacity>
       </View>
@@ -63,5 +114,27 @@ const styles = StyleSheet.create({
   buttonText: {
     fontSize: 18,
     color: "white",
+  },
+  cancelButton: {
+    width: 70,
+    paddingVertical: 5,
+    borderRadius: 5,
+    backgroundColor: "#e89415",
+    alignItems: "center",
+    alignSelf: "center",
+  },
+
+  outerView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)'
+  },
+  modalView: {
+    backgroundColor: 'white',
+    width: 250,
+    height: 220,
+    padding: 30,
+    borderRadius: 20
   },
 });
